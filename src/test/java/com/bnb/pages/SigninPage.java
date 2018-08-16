dd96d1b3f8029ce2a180eec44c922732ab589c13
Aybolek <aybolek27@gmail.com>
2018-08-16 14:55:50 -0500

    added signin page

---
+++ src/test/java/com/bnb/pages/SigninPage.java
@@ -0,0 +1,46 @@
+package com.bnb.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.bnb.utilities.Driver;
+
+public class SigninPage {
+
+	public SigninPage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+
+	@FindBy(name = "email")
+	public WebElement emailField;
+
+	@FindBy(name = "password")
+	public WebElement passwordField;
+
+	@FindBy(xpath = "//button[.='sign in']")
+	public WebElement signinButton;
+
+	@FindBy(xpath = "//h1[.='VA']")
+	public WebElement title;
+
+	@FindBy(xpath = "//h2[@class='subtitle']")
+	public WebElement subtitle;
+
+	@FindBy(xpath = "//a[.='my']")
+	public WebElement myButton;
+
+	@FindBy(xpath = "//a[.='self']")
+	public WebElement selfButton;
+	
+	
+	  public void login(String username, String password ) {
+		  emailField.sendKeys(username);
+		  passwordField.sendKeys(password);
+		  signinButton.click();
+		 
+	  }
+	
+	
+
+}
