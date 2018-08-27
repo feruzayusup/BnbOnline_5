baeffcda597ae22efbead4c9ea30df4f469173cd
mehmetucar86@gmail.com <mehmetucar86@gmail.com>
2018-08-26 23:33:12 -0500

    Teams page_defs and teams pages are updated

--- src/test/java/com/bnb/pages/TeamPage.java
+++ src/test/java/com/bnb/pages/TeamPage.java
@@ -16,9 +16,27 @@ public class TeamPage {
 	@FindBy(className="subtitle")
 	public WebElement teamName;
 	
+	@FindBy(className="title")
+	public WebElement title;
+	
 	@FindBy(xpath="//p[.='name']/preceding-sibling::p[@class='title is-6']")
 	public List<WebElement> teamMemberNames;
 
 	@FindBy(xpath="//p[.='role']/preceding-sibling::p[@class='title is-6']")
 	public List<WebElement> teamMemberRoles;
+	
+	
+	public boolean isUserIncluded(String firstName, String lastName) {
+		String fullName= firstName+" "+lastName;
+		
+		for (WebElement webElement : teamMemberNames) {
+			if(webElement.getText().equals(fullName))
+				return true;
+		}
+		
+		return false;
+		
+		
+	}
+	
 }
